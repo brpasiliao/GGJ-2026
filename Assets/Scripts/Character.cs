6a0be6f4ff20a07d42bbea8b96b024dfafa298d3
brpasiliao <brpasiliao@gmail.com>
2026-01-31 01:10:12 -0500

    created scenes

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 
 public class Character : MonoBehaviour {
     private static Character instance;
     public static Character Instance => instance;
+
+	[SerializeField] private Collider2D interactTrigger;
+	private List<Collider2D> interactableColliders;
 
     [SerializeField] private float speed;
 	private Vector3 facingVector;
@@ -15,13 +20,17 @@ public class Character : MonoBehaviour {
 
 	private void Awake() {
         instance = this;
+		interactableColliders = new();
 		facingVector = Vector3.down;
+		ammoCount = 0;
+		health = 1;
 	}
 
 	private void Update() {
 		HandleMovement();
+		HandleInteract();
 		HandleShooting();
-		Debug();
+		HandleDebug();
 	}
 
 	private void HandleMovement() {
@@ -46,6 +55,32 @@ public class Character : MonoBehaviour {
 		facingVector = normalizedVector == Vector3.zero ? facingVector : normalizedVector;
 	}
 
+	private void HandleInteract() {
+		List<Collider2D> colliders = new ();
+		interactTrigger.Overlap(colliders);
+
+		foreach (Collider2D collider in colliders) {
+			if (collider.TryGetComponent(out IInteractable interactable)) {
+				interactable.StartInteract();
+				if (!interactableColliders.Contains(collider)) {
+					interactableColliders.Add(collider);
+				}
+
+				if (Input.GetKeyDown(KeyCode.E)) {
+					interactable.Interact();
+				}
+			}
+		}
+
+		foreach (Collider2D collider in interactableColliders) {
+			if (!colliders.Contains(collider)) {
+				IInteractable interactable = collider.GetComponent<IInteractable>();
+				interactable.StopInteract();
+			}
+		}
+		interactableColliders.RemoveAll(collider => !colliders.Contains(collider));
+	}
+
 	private void HandleShooting() {
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			if (ammoCount > 0) {
@@ -54,7 +89,7 @@ public class Character : MonoBehaviour {
 		}
 	}
 
-	private void Debug() {
+	private void HandleDebug() {
 		if (Input.GetKeyDown(KeyCode.Alpha1)) {
 			GetAmmo();
 		}
